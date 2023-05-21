dfebb2c977577a2f6177051813eb9263c97536b2
SriBalaji <iam.thecloverly@pm.me>
2023-05-21 18:56:27 +0000

    RM6785: Include hardware/mediatek

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ rm -rf vendor/realme
 rm -rf kernel/realme/mt6785
 rm -rf realme/realme-firmwar
 rm -rf device/mediatek/sepolicy
+rm -rf hardware/mediatek 
 
 # clone
 git clone https://github.com/iamthecloverly/android_device_realme_RM6785 -b Elixir_3.0 device/realme/RM6785
@@ -12,7 +13,7 @@ git clone https://github.com/realme-mt6785-devs/android_kernel_realme_mt6785.git
 git clone https://github.com/realme-mt6785-devs/proprietary_vendor_realme-firmware realme/realme-firmware
 git clone https://github.com/realme-mt6785-devs/android_device_mediatek_sepolicy.git device/mediatek/sepolicy
 git clone https://github.com/realme-mt6785-devs/android_device_realme_RM6785-kernel.git device/realme/RM6785-kernel
-git clone https://github.com/realme-mt6785-devs/vendor_mediatek_opensource_interfaces.git vendor/mediatek/opensource/interfaces
+git clone https://github.com/realme-mt6785-devs/android_device_mediatek_sepolicy_vndr.git hardware/mediatek
 
 cd external/faceunlock && wget https://pastebin.com/raw/EbesA2Wm && patch -p1  < EbesA2Wm
 #patch for brightness slider 
