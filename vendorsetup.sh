254303451e0e7fdca42971df15a676b230fc7f0b
Sribalaji Annamalai Senthilkumar <iam.thecloverly@pm.me>
2023-07-06 22:24:35 +0530

    RM6785: Update vendorsetup.sh (#1)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,10 @@ rm -rf hardware/mediatek
 
 #clone 
 git clone https://github.com/iamthecloverly/local_manifests.git --depth 1 -b aex .repo/local_manifests
+
+# patch for external/faceunlock
+cd external/faceunlock && wget https://pastebin.com/raw/EbesA2Wm && patch -p1  < EbesA2Wm
+
+#patch for brightness slider 
+cd frameworks/base && wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices-a12l.patch
+patch -p1 < *.patch && cd -
